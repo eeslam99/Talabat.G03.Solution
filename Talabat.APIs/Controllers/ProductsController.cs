bf90e5569592f57680f27e162b3fae37b9aecc2e
Gaming Store <Gaming Store@DESKTOP-6FL3FUL>
2024-04-17 15:07:19 +0200

    Refactor Get Products

--- Talabat.APIs/Controllers/ProductsController.cs
+++ Talabat.APIs/Controllers/ProductsController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Talabat.Core.Repositories;
 using Talabat.Core.Entities;
+using Talabat.Core.Specifications;
+using Talabat.Core.ProductSpecifications;
 
 namespace Talabat.APIs.Controllers
 {
@@ -18,7 +20,8 @@ namespace Talabat.APIs.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Product>>> GetProducts() 
         {
-            var Products = await _productRepo.GetAllAsync();
+            var spec = new ProductWithBrandAndCategorySpecifications();
+            var Products = await _productRepo.GetAllWithSpecAsync(spec);
             return Ok(Products);
         }
 
---
+++ Talabat.Core/ProductSpecifications/ProductWithBrandAndCategorySpecifications.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Specifications;
+
+namespace Talabat.Core.ProductSpecifications
+{
+    public class ProductWithBrandAndCategorySpecifications : BaseSpecifications<Product>
+    {   
+        //This constructor will be used for creating an object, that will be used to get all product 
+        public ProductWithBrandAndCategorySpecifications() :base()
+        {
+            Includes.Add(P => P.Brand);
+            Includes.Add(P => P.Category);
+        }
+    }
+}
